44abc6fcbd726137f0a62f64f83e23d12866eec4
David Brunel <dkdev3185@gmail.com>
2021-09-23 10:42:15 +0200

    carousel

--- WeAreCoachBack/src/Controller/HomeController.php
+++ WeAreCoachBack/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\WorkoutRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,13 +13,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(WorkoutRepository $workoutRepository): Response
+    public function index(WorkoutRepository $workoutRepository, CategoryRepository $categoryRepository): Response
     {
+    $categoryList = $categoryRepository->findAll();
 
+    // We find the last 3 training sessions
     $latestWorkout = $workoutRepository->findBy([], ['id'=>'DESC'], 3); 
-
+    
+    // We send the result at the view
     return $this->render('home/index.html.twig', [
         'latestWorkout' => $latestWorkout,
+        'categoryList' => $categoryList,
         ]);
 
     
