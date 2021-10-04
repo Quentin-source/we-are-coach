e8fa681cebfe2120580b189f1ee9c312b354cc70
Guillaume Marie <pro.guillaume.marie@gmail.com>
2021-10-04 15:06:56 +0200

    correction search

--- WeAreCoachBack/src/Controller/Api/FavoriteController.php
+++ WeAreCoachBack/src/Controller/Api/FavoriteController.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Favorite;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Repository\FavoriteRepository;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
+/**
+     * @Route("/api/favorite", name="api_favorite_")
+     */
 class FavoriteController extends AbstractController
 {
     /**
-     * @Route("/api/favorite", name="api_favorite")
+     * @Route("/", name="favorite", methods={"GET"})
      */
-    public function index(): Response
+    public function index(FavoriteRepository $favoriteRepository): Response
     {
-        return $this->render('api/favorite/index.html.twig', [
-            'controller_name' => 'FavoriteController',
+        $favorite = $favoriteRepository->findAll();
+
+        return $this->json($favorite, 200, [], [
+            'groups' => 'favorite_list'
         ]);
     }
 }
--- WeAreCoachBack/src/Entity/Favorite.php
+++ WeAreCoachBack/src/Entity/Favorite.php
@@ -16,6 +16,7 @@ class Favorite
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      * 
+     * 
      */
     private $id;
 
